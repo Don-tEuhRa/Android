d1c65cbb18d7eddd283b3fc86adc89a6caa047d4
Dongmin <pdm001125@naver.com>
2023-07-26 20:55:24 +0900

    [FEAT] Font 적용
    공통 Font로 "SpoqaHansNeo" 적용.

--- app/src/main/java/com/dongminpark/reborn/ui/theme/Type.kt
+++ app/src/main/java/com/dongminpark/reborn/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.dongminpark.reborn.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dongminpark.reborn.R
+
+val customFamily = FontFamily(
+    Font(R.font.spopahansneo_thin, FontWeight.Thin),
+    Font(R.font.spopahansneo_light, FontWeight.Light),
+    Font(R.font.spopahansneo_medium, FontWeight.Medium),
+    Font(R.font.spopahansneo_bold, FontWeight.Bold),
+    Font(R.font.spopahansneo_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = customFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
