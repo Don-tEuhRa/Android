0837af94b07ce9786e12f4cb62ccf510030e2443
Dongmin <pdm001125@naver.com>
2023-08-07 20:37:36 +0900

    [FEAT] CheckBoxButton icon 변경
    CheckBoxButton icon 변경

--- app/src/main/java/com/dongminpark/reborn/Buttons/CheckBoxButton.kt
+++ app/src/main/java/com/dongminpark/reborn/Buttons/CheckBoxButton.kt
@@ -1,19 +1,18 @@
 package com.dongminpark.reborn.Buttons
 
-import androidx.compose.foundation.clickable
+
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.dongminpark.reborn.R
 
 @Composable
 fun CheckBoxButton(onClick: () -> Unit){
     var isChecked by remember { mutableStateOf(false) }
-    var icon = if (isChecked) Icons.Filled.CheckCircle else Icons.Outlined.CheckCircle
+    val icon = if (isChecked) R.drawable.checkcircle_solid else R.drawable.checkcircle_outline
 
     ButtonFormat(modifier = Modifier.padding(12.dp), icon = icon){
         isChecked = !isChecked
