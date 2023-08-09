fd9d5e779094d4889ce231fa79b91c4509ad832e
Dongmin <pdm001125@naver.com>
2023-08-09 10:52:46 +0900

    [FEAT] StoreAfterScreen 감사인사 추가
    StoreAfterScreen
    - 화면 상단 감사인사 추가

--- app/src/main/java/com/dongminpark/reborn/Screens/Store/StorePayAfterScreen.kt
+++ app/src/main/java/com/dongminpark/reborn/Screens/Store/StorePayAfterScreen.kt
@@ -1,15 +1,13 @@
 package com.dongminpark.reborn.Screens.Store
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,15 +25,22 @@ fun StorePayAfterScreen(navController: NavController) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(12.dp)
+                .padding(12.dp),
+            verticalArrangement = Arrangement.SpaceAround
         ){
+            item {
+                Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+                    TextFormat(text = "주문해 주셔서 감사합니다.", size = 28)
+                    TextFormat(text = "구매금액의 5%는 환경보호를 위해 기부됩니다", size = 16)
+                    TextFormat(text = "리본과 함께 나눔에 동참해주셔서 감사합니다.", size = 16)
+                }
+            }
+
             item{
                 UserInfoFrame(name = "박동민", phone = "010-2245-3683", address = "수원시 장안구 연무동 123-456")
             }
 
-            item {
-                Spacer(modifier = Modifier.padding(12.dp))
-            }
+
 
             item {
                 TextFormat(text = "상품 정보")
@@ -43,10 +48,6 @@ fun StorePayAfterScreen(navController: NavController) {
                 itemList.forEach{item ->
                     ItemInfoFrame(item = item.toString())
                 }
-            }
-
-            item {
-                Spacer(modifier = Modifier.padding(12.dp))
             }
 
             item {
